2f31e9bfd7c7a5d12c9502b66e2008ff95173796
Shrinidhi <shrinidhi.21810366@viit.ac.in>
2020-10-27 16:05:49 +0530

    about us

--- include/scripts.php
+++ include/scripts.php
@@ -20,6 +20,257 @@
                   $('nav').removeClass('black');
             }
       })
+!function(t,e){"function"==typeof define&&define.amd?define("ev-emitter/ev-emitter",e):"object"==typeof module&&module.exports?module.exports=e():t.EvEmitter=e()}("undefined"!=typeof window?window:this,function(){function t(){}var e=t.prototype;return e.on=function(t,e){if(t&&e){var i=this._events=this._events||{},n=i[t]=i[t]||[];return-1==n.indexOf(e)&&n.push(e),this}},e.once=function(t,e){if(t&&e){this.on(t,e);var i=this._onceEvents=this._onceEvents||{},n=i[t]=i[t]||{};return n[e]=!0,this}},e.off=function(t,e){var i=this._events&&this._events[t];if(i&&i.length){var n=i.indexOf(e);return-1!=n&&i.splice(n,1),this}},e.emitEvent=function(t,e){var i=this._events&&this._events[t];if(i&&i.length){var n=0,o=i[n];e=e||[];for(var r=this._onceEvents&&this._onceEvents[t];o;){var s=r&&r[o];s&&(this.off(t,o),delete r[o]),o.apply(this,e),n+=s?0:1,o=i[n]}return this}},t}),function(t,e){"use strict";"function"==typeof define&&define.amd?define(["ev-emitter/ev-emitter"],function(i){return e(t,i)}):"object"==typeof module&&module.exports?module.exports=e(t,require("ev-emitter")):t.imagesLoaded=e(t,t.EvEmitter)}(window,function(t,e){function i(t,e){for(var i in e)t[i]=e[i];return t}function n(t){var e=[];if(Array.isArray(t))e=t;else if("number"==typeof t.length)for(var i=0;i<t.length;i++)e.push(t[i]);else e.push(t);return e}function o(t,e,r){return this instanceof o?("string"==typeof t&&(t=document.querySelectorAll(t)),this.elements=n(t),this.options=i({},this.options),"function"==typeof e?r=e:i(this.options,e),r&&this.on("always",r),this.getImages(),h&&(this.jqDeferred=new h.Deferred),void setTimeout(function(){this.check()}.bind(this))):new o(t,e,r)}function r(t){this.img=t}function s(t,e){this.url=t,this.element=e,this.img=new Image}var h=t.jQuery,a=t.console;o.prototype=Object.create(e.prototype),o.prototype.options={},o.prototype.getImages=function(){this.images=[],this.elements.forEach(this.addElementImages,this)},o.prototype.addElementImages=function(t){"IMG"==t.nodeName&&this.addImage(t),this.options.background===!0&&this.addElementBackgroundImages(t);var e=t.nodeType;if(e&&d[e]){for(var i=t.querySelectorAll("img"),n=0;n<i.length;n++){var o=i[n];this.addImage(o)}if("string"==typeof this.options.background){var r=t.querySelectorAll(this.options.background);for(n=0;n<r.length;n++){var s=r[n];this.addElementBackgroundImages(s)}}}};var d={1:!0,9:!0,11:!0};return o.prototype.addElementBackgroundImages=function(t){var e=getComputedStyle(t);if(e)for(var i=/url\((['"])?(.*?)\1\)/gi,n=i.exec(e.backgroundImage);null!==n;){var o=n&&n[2];o&&this.addBackground(o,t),n=i.exec(e.backgroundImage)}},o.prototype.addImage=function(t){var e=new r(t);this.images.push(e)},o.prototype.addBackground=function(t,e){var i=new s(t,e);this.images.push(i)},o.prototype.check=function(){function t(t,i,n){setTimeout(function(){e.progress(t,i,n)})}var e=this;return this.progressedCount=0,this.hasAnyBroken=!1,this.images.length?void this.images.forEach(function(e){e.once("progress",t),e.check()}):void this.complete()},o.prototype.progress=function(t,e,i){this.progressedCount++,this.hasAnyBroken=this.hasAnyBroken||!t.isLoaded,this.emitEvent("progress",[this,t,e]),this.jqDeferred&&this.jqDeferred.notify&&this.jqDeferred.notify(this,t),this.progressedCount==this.images.length&&this.complete(),this.options.debug&&a&&a.log("progress: "+i,t,e)},o.prototype.complete=function(){var t=this.hasAnyBroken?"fail":"done";if(this.isComplete=!0,this.emitEvent(t,[this]),this.emitEvent("always",[this]),this.jqDeferred){var e=this.hasAnyBroken?"reject":"resolve";this.jqDeferred[e](this)}},r.prototype=Object.create(e.prototype),r.prototype.check=function(){var t=this.getIsImageComplete();return t?void this.confirm(0!==this.img.naturalWidth,"naturalWidth"):(this.proxyImage=new Image,this.proxyImage.addEventListener("load",this),this.proxyImage.addEventListener("error",this),this.img.addEventListener("load",this),this.img.addEventListener("error",this),void(this.proxyImage.src=this.img.src))},r.prototype.getIsImageComplete=function(){return this.img.complete&&void 0!==this.img.naturalWidth},r.prototype.confirm=function(t,e){this.isLoaded=t,this.emitEvent("progress",[this,this.img,e])},r.prototype.handleEvent=function(t){var e="on"+t.type;this[e]&&this[e](t)},r.prototype.onload=function(){this.confirm(!0,"onload"),this.unbindEvents()},r.prototype.onerror=function(){this.confirm(!1,"onerror"),this.unbindEvents()},r.prototype.unbindEvents=function(){this.proxyImage.removeEventListener("load",this),this.proxyImage.removeEventListener("error",this),this.img.removeEventListener("load",this),this.img.removeEventListener("error",this)},s.prototype=Object.create(r.prototype),s.prototype.check=function(){this.img.addEventListener("load",this),this.img.addEventListener("error",this),this.img.src=this.url;var t=this.getIsImageComplete();t&&(this.confirm(0!==this.img.naturalWidth,"naturalWidth"),this.unbindEvents())},s.prototype.unbindEvents=function(){this.img.removeEventListener("load",this),this.img.removeEventListener("error",this)},s.prototype.confirm=function(t,e){this.isLoaded=t,this.emitEvent("progress",[this,this.element,e])},o.makeJQueryPlugin=function(e){e=e||t.jQuery,e&&(h=e,h.fn.imagesLoaded=function(t,e){var i=new o(this,t,e);return i.jqDeferred.promise(h(this))})},o.makeJQueryPlugin(),o});
+
+
+const MathUtils = {
+        // map number x from range [a, b] to [c, d]
+        map: (x, a, b, c, d) => (x - a) * (d - c) / (b - a) + c,
+        // linear interpolation
+        lerp: (a, b, n) => (1 - n) * a + n * b,
+        // Random float
+        getRandomFloat: (min, max) => (Math.random() * (max - min) + min).toFixed(2)
+    };
+
+    // body element
+    const body = document.body;
+    
+    // calculate the viewport size
+    let winsize;
+    const calcWinsize = () => winsize = {width: window.innerWidth, height: window.innerHeight};
+    calcWinsize();
+    // and recalculate on resize
+    window.addEventListener('resize', calcWinsize);
+    
+    // scroll position
+    let docScroll;
+    // for scroll speed calculation
+    let lastScroll;
+    let scrollingSpeed = 0;
+    // scroll position update function
+    const getPageYScroll = () => docScroll = window.pageYOffset || document.documentElement.scrollTop;
+    window.addEventListener('scroll', getPageYScroll);
+
+    // Item
+    class Item {
+        constructor(el) {
+            // the .item element
+            this.DOM = {el: el};
+            // the inner image
+            this.DOM.image = this.DOM.el.querySelector('.content__item-img');
+            this.DOM.imageWrapper = this.DOM.image.parentNode;
+            this.DOM.title = this.DOM.el.querySelector('.content__item-title');
+            this.renderedStyles = {
+                // here we define which property will change as we scroll the page and the item is inside the viewport
+                // in this case we will be:
+                // - scaling the inner image
+                // - translating the item's title
+                // we interpolate between the previous and current value to achieve a smooth effect
+                imageScale: {
+                    // interpolated value
+                    previous: 0, 
+                    // current value
+                    current: 0, 
+                    // amount to interpolate
+                    ease: 0.1,
+                    // current value setter
+                    setValue: () => {
+                        const toValue = 1.5;
+                        const fromValue = 1;
+                        const val = MathUtils.map(this.props.top - docScroll, winsize.height, -1 * this.props.height, fromValue, toValue);
+                        return Math.max(Math.min(val, toValue), fromValue);
+                    }
+                },
+                titleTranslationY: {
+                    previous: 0, 
+                    current: 0, 
+                    ease: 0.1,
+                    fromValue: Number(MathUtils.getRandomFloat(30,400)),
+                    setValue: () => {
+                        const fromValue = this.renderedStyles.titleTranslationY.fromValue;
+                        const toValue = -1*fromValue;
+                        const val = MathUtils.map(this.props.top - docScroll, winsize.height, -1 * this.props.height, fromValue, toValue);
+                        return fromValue < 0 ? Math.min(Math.max(val, fromValue), toValue) : Math.max(Math.min(val, fromValue), toValue);
+                    }
+                }
+            };
+            // gets the item's height and top (relative to the document)
+            this.getSize();
+            // set the initial values
+            this.update();
+            // use the IntersectionObserver API to check when the element is inside the viewport
+            // only then the element styles will be updated
+            this.observer = new IntersectionObserver((entries) => {
+                entries.forEach(entry => this.isVisible = entry.intersectionRatio > 0);
+            });
+            this.observer.observe(this.DOM.el);
+            // init/bind events
+            this.initEvents();
+        }
+        update() {
+            // sets the initial value (no interpolation)
+            for (const key in this.renderedStyles ) {
+                this.renderedStyles[key].current = this.renderedStyles[key].previous = this.renderedStyles[key].setValue();
+            }
+            // apply changes/styles
+            this.layout();
+        }
+        getSize() {
+            const rect = this.DOM.el.getBoundingClientRect();
+            this.props = {
+                // item's height
+                height: rect.height,
+                // offset top relative to the document
+                top: docScroll + rect.top
+            }
+        }
+        initEvents() {
+            window.addEventListener('resize', () => this.resize());
+        }
+        resize() {
+            // gets the item's height and top (relative to the document)
+            this.getSize();
+            // on resize reset sizes and update styles
+            this.update();
+        }
+        render() {
+            // update the current and interpolated values
+            for (const key in this.renderedStyles ) {
+                this.renderedStyles[key].current = this.renderedStyles[key].setValue();
+                this.renderedStyles[key].previous = MathUtils.lerp(this.renderedStyles[key].previous, this.renderedStyles[key].current, this.renderedStyles[key].ease);
+            }
+            
+            // and apply changes
+            this.layout();
+        }
+        layout() {
+            // scale the image
+            this.DOM.image.style.transform = `scale3d(${this.renderedStyles.imageScale.previous},${this.renderedStyles.imageScale.previous},1)`;
+            // translate the title
+            this.DOM.title.style.transform = `translate3d(0,${this.renderedStyles.titleTranslationY.previous}px,0)`;
+        }
+    }
+
+    // SmoothScroll
+    class SmoothScroll {
+        constructor() {
+            // the <main> element
+            this.DOM = {main: document.querySelector('main')};
+            // the scrollable element
+            // we translate this element when scrolling (y-axis)
+            this.DOM.scrollable = this.DOM.main.querySelector('div[data-scroll]');
+            // the items on the page
+            this.items = [];
+            this.DOM.content = this.DOM.main.querySelector('.content');
+            [...this.DOM.content.querySelectorAll('.content__item')].forEach(item => this.items.push(new Item(item)));
+            // here we define which property will change as we scroll the page
+            // in this case we will be translating on the y-axis
+            // we interpolate between the previous and current value to achieve the smooth scrolling effect
+            this.renderedStyles = {
+                translationY: {
+                    // interpolated value
+                    previous: 0, 
+                    // current value
+                    current: 0, 
+                    // amount to interpolate
+                    ease: 0.1,
+                    // current value setter
+                    // in this case the value of the translation will be the same like the document scroll
+                    setValue: () => docScroll
+                }
+            };
+            // set the body's height
+            this.setSize();
+            // set the initial values
+            this.update();
+            // the <main> element's style needs to be modified
+            this.style();
+            // init/bind events
+            this.initEvents();
+            // start the render loop
+            requestAnimationFrame(() => this.render());
+        }
+        update() {
+            // sets the initial value (no interpolation) - translate the scroll value
+            for (const key in this.renderedStyles ) {
+                this.renderedStyles[key].current = this.renderedStyles[key].previous = this.renderedStyles[key].setValue();   
+            }   
+            // translate the scrollable element
+            this.layout();
+        }
+        layout() {
+            this.DOM.scrollable.style.transform = `translate3d(0,${-1*this.renderedStyles.translationY.previous}px,0)`;
+        }
+        setSize() {
+            // set the heigh of the body in order to keep the scrollbar on the page
+            body.style.height = `${this.DOM.scrollable.scrollHeight}px`;
+        }
+        style() {
+            // the <main> needs to "stick" to the screen and not scroll
+            // for that we set it to position fixed and overflow hidden 
+            this.DOM.main.style.position = 'fixed';
+            this.DOM.main.style.width = this.DOM.main.style.height = '100%';
+            this.DOM.main.style.top = this.DOM.main.style.left = 0;
+            this.DOM.main.style.overflow = 'hidden';
+        }
+        initEvents() {
+            // on resize reset the body's height
+            window.addEventListener('resize', () => this.setSize());
+        }
+        render() {
+            // Get scrolling speed
+            // Update lastScroll
+            scrollingSpeed = Math.abs(docScroll - lastScroll);
+            lastScroll = docScroll;
+            
+            // update the current and interpolated values
+            for (const key in this.renderedStyles ) {
+                this.renderedStyles[key].current = this.renderedStyles[key].setValue();
+                this.renderedStyles[key].previous = MathUtils.lerp(this.renderedStyles[key].previous, this.renderedStyles[key].current, this.renderedStyles[key].ease);    
+            }
+            // and translate the scrollable element
+            this.layout();
+            
+            // for every item
+            for (const item of this.items) {
+                // if the item is inside the viewport call it's render function
+                // this will update item's styles, based on the document scroll value and the item's position on the viewport
+                if ( item.isVisible ) {
+                    if ( item.insideViewport ) {
+                        item.render();
+                    }
+                    else {
+                        item.insideViewport = true;
+                        item.update();
+                    }
+                }
+                else {
+                    item.insideViewport = false;
+                }
+            }
+            
+            // loop..
+            requestAnimationFrame(() => this.render());
+        }
+    }
+
+  
+    const preloadImages = () => {
+        return new Promise((resolve, reject) => {
+            imagesLoaded(document.querySelectorAll('.content__item-img'), {background: true}, resolve);
+        });
+    };
+    
+    // And then..
+    preloadImages().then(() => {
+        // Remove the loader
+        document.body.classList.remove('loading');
+        // Get the scroll position and update the lastScroll variable
+        getPageYScroll();
+        lastScroll = docScroll;
+        // Initialize the Smooth Scrolling
+        new SmoothScroll();
+    });
       </script>
 </body>
 
